54bacfdf05864270a90921509b09b97c8d10ecbd
suhaybzz <suhaybahmed07@gmail.com>
2025-11-23 23:12:32 +0000

    gold smith user added

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -3,3 +3,6 @@
 USE berties_books;
 
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
+
+INSERT INTO users (username, first, last, email, hashedPassword)
+VALUES ('gold', 'Gold', 'Smiths', 'gold@example.com', '$2b$10$UsD0B/QYcclaoAR4AbT4MOUnpdlSEN47Y6JuzoIpLCfqIzTwm1Qte');
